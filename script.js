b3eac2b79e0338f79a92ff71b056df44a3b10a91
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-25 15:32:49 +0530

    second try

--- script.js
+++ script.js
@@ -1,60 +1,46 @@
-//have a console on mobile
-//const consoleOutput = document.getElementById("console");
-const log = function (msg) {
- // consoleOutput.innerText = `${consoleOutput.innerText}\n${msg}`;
-  console.log(msg);
-}
-
 //Test browser support
 const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
 
 if (SUPPORTS_MEDIA_DEVICES) {
   //Get the environment camera (usually the second one)
   navigator.mediaDevices.enumerateDevices().then(devices => {
-
+  
     const cameras = devices.filter((device) => device.kind === 'videoinput');
 
     if (cameras.length === 0) {
-      log('No camera found on this device.');
+      throw 'No camera found on this device.';
     }
+    const camera = cameras[cameras.length - 1];
+
     // Create stream and get video track
     navigator.mediaDevices.getUserMedia({
       video: {
-        facingMode: 'environment',
+        deviceId: camera.deviceId,
+        facingMode: ['user', 'environment'],
+        height: {ideal: 1080},
+        width: {ideal: 1920}
       }
     }).then(stream => {
       const track = stream.getVideoTracks()[0];
 
       //Create image capture object and get camera capabilities
       const imageCapture = new ImageCapture(track)
-      imageCapture.getPhotoCapabilities().then(capabilities => {
+      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+
+        //todo: check if camera has a torch
+
         //let there be light!
         const btn = document.querySelector('.switch');
-        const torchSupported = !!capabilities.torch || (
-          'fillLightMode' in capabilities &&
-          capabilities.fillLightMode.length != 0 &&
-          capabilities.fillLightMode != 'none'
-        );
-
-        if (torchSupported) {
-          let torch = false;
-          btn.addEventListener('click', function (e) {
-            try {
-              track.applyConstraints({
-                advanced: [{
-                  torch: (torch = !torch)
-                }]
-              });
-            } catch (err) {
-              log(err);
-            }
+        btn.addEventListener('click', function(){
+          track.applyConstraints({
+            advanced: [{torch: true}]
           });
-        } else {
-          log("No torch found");
-        }
-      }).catch(log);
-    }).catch(log);
-  }).catch(log);
-
+        });
+      });
+    });
+  });
+  
   //The light will be on as long the track exists
+  
+  
 }
